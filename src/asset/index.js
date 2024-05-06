6f9de44578e3289b262e997e7ba79adea7225f1d
anas2357 <141234155+anas2357@users.noreply.github.com>
2024-05-06 23:13:48 +0530

    did something

---
+++ src/asset/index.js
@@ -0,0 +1,14 @@
+
+const Smoothe1 = require("@/asset/Smoothe-1.png");
+const Smoothe2 = require("@/asset/Smoothe-2.png");
+const Smoothe3 = require("@/asset/Smoothe-3.png");
+const Smoothe4 = require("@/asset/Smoothe-4.png");
+
+const images = {
+  Smoothe1,
+  Smoothe2,
+  Smoothe3,
+  Smoothe4,
+};
+
+export default images;
