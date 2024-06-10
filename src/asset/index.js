62f846f4570dfeeaf51c155f3ef384d73acf58ed
anas2357 <141234155+anas2357@users.noreply.github.com>
2024-06-10 14:46:51 +0530

    Add rest of the page

--- src/asset/index.js
+++ src/asset/index.js
@@ -3,12 +3,50 @@ const Smoothe1 = require("@/asset/Smoothe-1.png");
 const Smoothe2 = require("@/asset/Smoothe-2.png");
 const Smoothe3 = require("@/asset/Smoothe-3.png");
 const Smoothe4 = require("@/asset/Smoothe-4.png");
+const Star = require("@/asset/Star-Blur.png");
+const Banana1 = require("@/asset/Banana1.png");
+const Banana2 = require("@/asset/Banana2.png");
+const Banana3 = require("@/asset/Banana3.png");
+const Berry1 = require("@/asset/Berry1.png");
+const Berry2 = require("@/asset/Berry2.png");
+const Chocolate1 = require("@/asset/Chocolate1.png");
+const Chocolate2 = require("@/asset/Chocolate2.png");
+const Chocolate3 = require("@/asset/Chocolate3.png");
+const KaleLeaf = require("@/asset/KaleLeaf.png");
+const Mango1 = require("@/asset/Mango1.png");
+const Mango2 = require("@/asset/Mango2.png");
+const Mango3 = require("@/asset/Mango3.png");
+const Strawberry = require("@/asset/Strawberry.png");
+const Watermelon = require("@/asset/Watermelon.png");
+const Sugar = require("@/asset/Sugar.svg");
+const Protein = require("@/asset/Protein.svg");
+const Fiber = require("@/asset/Fiber.svg");
+
+
 
 const images = {
   Smoothe1,
   Smoothe2,
   Smoothe3,
   Smoothe4,
+  Star,
+  Banana1,
+  Banana2,
+  Banana3,
+  Berry1,
+  Berry2,
+  Chocolate1,
+  Chocolate2,
+  Chocolate3,
+  KaleLeaf,
+  Mango1,
+  Mango2,
+  Mango3,
+  Strawberry,
+  Watermelon,
+  Sugar,
+  Protein,
+  Fiber,
 };
 
 export default images;
