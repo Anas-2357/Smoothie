6e76195101fbbfa2018863eaa48cb58fef8e6505
Anas <anasansari2357@gmail.com>
2024-08-05 13:10:37 +0530

    changed config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  images: {
+      unoptimized: true,
+  },
 };
 
 export default nextConfig;
